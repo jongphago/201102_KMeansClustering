3b159d205df6e40fc222ac8bfdf7f440bf643c53
Jong Hyun Kim <jonghyun.d.kim@gmail.com>
2020-11-02 20:27:41 +0900

    dataToArray:  함수 [dataToArray]는 "input.txt" 파일을 읽어서 점 데이터를 float형 2차원 배열(변수명:dataArray)에 저장합니다. printFloat2DArray: 함수 [printFloat2DArray]는 float type의 2차원 배열을 받아서 """index:  data1  data2  ...  dataK""" 형태로 출력합니다. 행의 수는 firstIndexCount, 열의 수는 secondIndexCount와 같습니다.

--- kmeans_120200199.c
+++ kmeans_120200199.c
@@ -1,7 +1,90 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
-void main(void) {
-	printf("Hello, Github");
+int dataCount = 0;
+int featureCount = 0;
+int K = 0;
+int scanfCount = 0;
+
+void scanfException(int scanfReturn, int scanfAnswer)
+{
+	if (scanfReturn != scanfAnswer)
+	{
+		printf("SCANF ERROR iS OCCURED.");
+		exit(-1);
+	}
 	return;
+}
+void dataToArray(FILE** inputFile, float*** dataArray);
+void printFloat2DArray(float** dataArray, int firstIndexCount, int secondIndexCount);
+
+
+
+void main(void) {
+	FILE* inputFile;
+	float** dataArray;
+
+	dataToArray(&inputFile, &dataArray);
+	//printf("%d %d %d\n", dataCount, featureCount, K);
+	//printFloat2DArray(dataArray, dataCount, featureCount);
+
+
+
+	return;
+}
+
+/*
+함수 [dataToArray]는 "input.txt" 파일을 읽어서 점 데이터를
+float형 2차원 배열(변수명:dataArray)에 저장합니다.
+*/
+void dataToArray(FILE** inputFile, float*** dataArray) {
+	*inputFile = fopen("input.txt", "r");
+	if ((*inputFile) == NULL)
+	{
+		printf("File Open Error");
+		exit(-1);
+	}
+	scanfCount = fscanf(*inputFile, "%d %d %d", &dataCount, &featureCount, &K);
+	if (scanfCount != 3)
+	{
+		printf("fscanf Error");
+		exit(-1);
+	}
+
+	*dataArray = malloc(dataCount * sizeof(float*));
+	for (int i = 0; i < dataCount; i++)
+	{
+		(*dataArray)[i] = malloc(featureCount * sizeof(float));
+		if ((*dataArray)[i] == NULL)
+		{
+			printf("MEMORY ALLOCATION ERROR.");
+			exit(-1);
+		}
+		for (int j = 0; j < featureCount; j++)
+		{
+			scanfCount = fscanf(*inputFile, "%f", &((*dataArray)[i][j]));
+			if (scanfCount != 1) {
+				exit(-1);
+			}
+			scanfException(scanfCount, 1);
+		}
+	}
+}
+
+/*
+함수 [printFloat2DArray]는 float type의 2차원 배열을 받아서 
+index:		data1		data2		...		dataK 형태로 출력합니다.
+행의 수는 firstIndexCount, 열의 수는 secondIndexCount와 같습니다.
+*/
+void printFloat2DArray(float** dataArray, int firstIndexCount, int secondIndexCount) {
+	for (int i = 0; i < firstIndexCount; i++)
+	{
+		printf("%3d:\t", i);
+		for (int j = 0; j < secondIndexCount; j++)
+		{
+			printf("%.3f\t", dataArray[i][j]);
+		}
+		printf("\n");
+	}
 }
---
+++ readme.txt
@@ -0,0 +1,12 @@
+[2020.10.26]
+K-Means Clustering 과제입니다.
+과제에 대한 설명이 10월 29일에 있을 예정입니다.
+KNN 과제와 마찬가지로 샘플 데이터를 첨부해 드립니다.
+채점은 새로운 데이터로 진행될 예정입니다.
+K-Means Clustering 알고리즘에는 랜덤성이 존재하기 때문에 반드시 random seed를 1000으로 고정하신 후 제출해주시기 바랍니다.
+
+[2020.10.29]
+K-Means Clustering 숙제 설명 파일에 빠진 부분이 있어서 추가합니다.
+- 데이터와 centroid의 거리는 L2 distance(Euclidean distance)로 측정한다.
+- Maximum iteration 수는 50으로 한다.
+- 만약 거리가 같은 centroid가 두개 이상일때는 군집의 index가 가장 낮은 군집으로 데이터를 포함시킨다.
